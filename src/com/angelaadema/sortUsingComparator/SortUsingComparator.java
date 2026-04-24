9987c634af5d7447e2cba3a72dfa34dc6b6855ac
Angi-Adema <dr.adema@gmail.com>
2026-04-24 14:20:56 -0600

    changed the order of the studentList to show that the sort works properly.

--- src/com/angelaadema/sortUsingComparator/SortUsingComparator.java
+++ src/com/angelaadema/sortUsingComparator/SortUsingComparator.java
@@ -10,16 +10,17 @@ public class SortUsingComparator {
 		ArrayList<Student> studentList = new ArrayList<>();
 		
 		// Add student objects to the ArrayList
-		studentList.add(new Student(100, "Sally", "Cobbleston Dr."));
-		studentList.add(new Student(101, "Vinnie", "Tulip St."));
-		studentList.add(new Student(102, "Dan", "Orange St."));
-		studentList.add(new Student(103, "Billie", "Decatur Cir."));
-		studentList.add(new Student(104, "Joe", "Troy St."));
-		studentList.add(new Student(105, "Patrick", "Rodeo Dr."));
-		studentList.add(new Student(106, "Robert", "Circleton St."));
 		studentList.add(new Student(107, "Tammy", "Jebel Wy."));
 		studentList.add(new Student(108, "Wilma", "Stone St."));
+		studentList.add(new Student(104, "Joe", "Troy St."));
+		studentList.add(new Student(100, "Sally", "Cobbleston Dr."));
+		studentList.add(new Student(105, "Patrick", "Rodeo Dr."));
+		studentList.add(new Student(102, "Dan", "Orange St."));
 		studentList.add(new Student(109, "Mary", "Vaughn St."));
+		studentList.add(new Student(101, "Vinnie", "Tulip St."));
+		studentList.add(new Student(103, "Billie", "Decatur Cir."));
+		studentList.add(new Student(106, "Robert", "Circleton St."));
+		
 		
 		// Sort the studentList by name using CustomSelectionSort and NameSort Comparator
 		CustomSelectionSort.sort(studentList, new NameSort());
