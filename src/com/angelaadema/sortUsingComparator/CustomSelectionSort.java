7bbdc64cce1c6c57ebd653e630a2c32558a4bba1
Angi-Adema <dr.adema@gmail.com>
2026-04-24 10:44:03 -0600

    updated key variable to minIndex variable name in CustomSelectionSort.java

--- src/com/angelaadema/sortUsingComparator/CustomSelectionSort.java
+++ src/com/angelaadema/sortUsingComparator/CustomSelectionSort.java
@@ -9,27 +9,27 @@ public class CustomSelectionSort {
 	// Sort method passing in ArrayList of student objects and Comparator
 	public static void sort(ArrayList<Student> studentList, Comparator<Student> comparator) {
 		
-		// Set length of array to variable length
+		// Store size of ArrayList in variable length
 		int length = studentList.size();
 		
-		// For loop to track the position of the key index and unsorted portion of array
+		// For loop to track the position of minIndex and unsorted portion of array
 		for (int i = 0; i < length - 1; i++) {
-			int key = i;
+			int minIndex = i;
 			
 			// Inner loop to track second value (j) being compared to i
 			for (int j = i + 1; j < length; j++) {
 				
 				// Use comparison value returned from Comparator to determine swap of objects
-				if (comparator.compare(studentList.get(j), studentList.get(key)) < 0) {
+				if (comparator.compare(studentList.get(j), studentList.get(minIndex)) < 0) {
 					
-					// Insert value of j into key index
-					key = j;
+					// Insert value of j into minIndex
+					minIndex = j;
 				}
 			}
 			
 			// Logic to swap the elements
-			Student tempIndex = studentList.get(key);
-			studentList.set(key,  studentList.get(i));
+			Student tempIndex = studentList.get(minIndex);
+			studentList.set(minIndex,  studentList.get(i));
 			studentList.set(i,  tempIndex);
 		}
 	}
