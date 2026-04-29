3499eec14949115721d268cbce30cd8daf13597c
Angi-Adema <dr.adema@gmail.com>
2026-04-29 11:19:42 -0600

    eliminated all the old commented out code.

--- src/com/angelaadema/sortUsingComparator/SortUsingComparator.java
+++ src/com/angelaadema/sortUsingComparator/SortUsingComparator.java
@@ -7,24 +7,9 @@ public class SortUsingComparator {
 
 	public static void main(String[] args) {
 		
-		// REFACTORED TO USE LIST AS TYPE INSTEAD OF ARRAYLIST AND MOVED INTO HELPER METHOD
-		// Create an ArrayList
-		//List<Student> studentList = new ArrayList<>();
-		List<Student> studentList = createStudentList();
-		
-		// REFACTORED INTO HELPER METHOD
-		// Add student objects to the ArrayList
-//		studentList.add(new Student(107, "Tammy", "Jebel Wy."));
-//		studentList.add(new Student(108, "Wilma", "Stone St."));
-//		studentList.add(new Student(104, "Joe", "Troy St."));
-//		studentList.add(new Student(100, "Sally", "Cobbleston Dr."));
-//		studentList.add(new Student(105, "Patrick", "Rodeo Dr."));
-//		studentList.add(new Student(102, "Dan", "Orange St."));
-//		studentList.add(new Student(109, "Mary", "Vaughn St."));
-//		studentList.add(new Student(101, "Vinnie", "Tulip St."));
-//		studentList.add(new Student(103, "Billie", "Decatur Cir."));
-//		studentList.add(new Student(106, "Robert", "Circleton St."));
-		
+		// REFACTORED TO USE LIST AS TYPE INSTEAD OF ARRAYLIST AND MOVED 
+		// CREATION OF STUDENT OBJECTS INTO HELPER METHOD
+		List<Student> studentList = createStudentList();		
 		
 		// Sort the studentList by name using CustomSelectionSort and NameSort Comparator
 		CustomSelectionSort.sort(studentList, new NameSort());
@@ -32,13 +17,7 @@ public class SortUsingComparator {
 		// Print statement to print list based on name
 		System.out.println("Objects sorted by name: ");
 		
-		// REFACTORED PRINTING INTO A HELPER FUNCTION ELIMINATING REPEATED LOOPS
-		// Use enhanced for loop to loop through and print ArrayList of objects
-//		for (Student student : studentList) {
-//			System.out.println(student);
-//		}
-		
-		// NEW PRINT STATEMENT
+		// REFACTORED PRINT STATEMENT
 		printStudentList(studentList);
 		
 		System.out.println();
@@ -49,13 +28,7 @@ public class SortUsingComparator {
 		// Print statement to print list based on roll number
 		System.out.println("Objects sorted by roll number: ");
 		
-		// REFACTORED TO ELIMINATE MULTIPLE PRINT LOOPS
-		// Use enhanced for loop to loop through and print ArrayList of objects
-//		for (Student student : studentList) {
-//			System.out.println(student);
-//		}
-		
-		// REUSE PRINT STATEMENT ABOVE
+		// REFACTORED PRINT STATEMENT
 		printStudentList(studentList);
 	}
 	
